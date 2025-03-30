0aa8e29c8abf73873dc26fad704d8e3a5a30733a
Ghaidaa <117023274+Ghaidaa2011@users.noreply.github.com>
2025-03-30 20:40:30 +0200

    done

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,31 +8,19 @@ interface MainProps {
 const Main = memo(({ timings }: MainProps) => {
   return (
     <Stack
-      sx={{ direction: "ltr", margin: "5vh 0px" }}
-      direction="row"
+      sx={{
+        direction: "ltr",
+        margin: "5vh 0px",
+      }}
+      direction="row-reverse"
       spacing={1}
       flexWrap="wrap"
       justifyContent={"space-around"}
     >
       <PrayerCard
-        name="العشاء"
-        time={timings?.Isha}
-        image="https://img.freepik.com/premium-photo/person-reflecting-preparing-spiritually-before-start-ramadan_1314467-209123.jpg?w=826"
-      />
-      <PrayerCard
-        name="المغرب"
-        time={timings?.Maghrib}
-        image="https://img.freepik.com/premium-photo/person-performing-fajr-prayer-with-tranquil-sunrise-background_1314467-209326.jpg"
-      />
-      <PrayerCard
-        name="العصر"
-        time={timings?.Asr}
-        image="https://img.freepik.com/premium-photo/person-meditating-praying-serene-setting-ramadan_1314467-211536.jpg?w=826"
-      />
-      <PrayerCard
-        name="الظهر"
-        time={timings?.Dhuhr}
-        image="https://img.freepik.com/premium-photo/peaceful-contemplative-setting-person-fasting-day_1314467-207873.jpg?w=826"
+        name="الفجر"
+        time={timings?.Fajr}
+        image="https://img.freepik.com/premium-photo/person-reflecting-preparing-spiritually-before-start-ramadan_1314467-208990.jpg?w=826"
       />
       <PrayerCard
         name="الشروق"
@@ -40,9 +28,24 @@ const Main = memo(({ timings }: MainProps) => {
         image="https://img.freepik.com/premium-photo/tranquil-mosque-scene-with-soft-lighting-worshippers-prayer_1314467-208529.jpg?w=826"
       />
       <PrayerCard
-        name="الفجر"
-        time={timings?.Fajr}
-        image="https://img.freepik.com/premium-photo/person-reflecting-preparing-spiritually-before-start-ramadan_1314467-208990.jpg?w=826"
+        name="الظهر"
+        time={timings?.Dhuhr}
+        image="https://img.freepik.com/premium-photo/peaceful-contemplative-setting-person-fasting-day_1314467-207873.jpg?w=826"
+      />
+      <PrayerCard
+        name="العصر"
+        time={timings?.Asr}
+        image="https://img.freepik.com/premium-photo/person-meditating-praying-serene-setting-ramadan_1314467-211536.jpg?w=826"
+      />
+      <PrayerCard
+        name="المغرب"
+        time={timings?.Maghrib}
+        image="https://img.freepik.com/premium-photo/person-performing-fajr-prayer-with-tranquil-sunrise-background_1314467-209326.jpg"
+      />
+      <PrayerCard
+        name="العشاء"
+        time={timings?.Isha}
+        image="https://img.freepik.com/premium-photo/person-reflecting-preparing-spiritually-before-start-ramadan_1314467-209123.jpg?w=826"
       />
     </Stack>
   );
